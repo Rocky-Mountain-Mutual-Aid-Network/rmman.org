d11ff03e7cc2c115b28caa23523f0e7ae2261b56
Drew Hornbein <drew@dhornbein.com>
2020-09-10 19:29:07 -0600

    Moves gdoc embedding logic to script

--- README.md
+++ README.md
@@ -2,12 +2,31 @@
 Rocky Mountain Mutual Aid Network
 
 This website is a super simple flat HTML site that is hosted (for free) using GitHub Pages. 
-Just copy the content out of index.html and swap out the Google Doc URL from the script at the bottom.
+If you wish to clone this design just fork the repository! 
 
 The page uses javascript to pull in the HTML of a Google Doc and display the `#content` div.
 
 If the page can't pull the content it instead loads an iframe of the content.
 
-## Preparing the Google Doc
+## Embedding Google Doc Content
 
-In the doc you want to use as your page content go to `file > publish` and publish the doc.
+This website makes use of published Googld Doc content. Javascript pulls the HTML of **published** Google Docs
+and displays the `#content` div (or the body content of the document). If it can't load the HTML content it 
+will gracefully fall back to an embedded iframe.
+
+To add Google Doc content you'll need to do the following:
+
+1. Publish your Google Doc - In the doc you want to use as your page content go to `file > publish` and publish the doc, save the link URL for later.
+2. add `<script src="/script-google-doc.js"></script>` to the page.
+3. add an element with the class `doc` and attribute `data-gdoc-url` with the published url
+
+``` html
+<div id="doc" data-gdoc-url="https://docs.google.com/document/d/e/2PACX-1vT5gCxJy7b1abHTQ0AKOFCYbssHDy1pVQyjJmBvsRrwA1T7GiULwaENsv2k_Mfwj8xYdBEiQzvJtD8N/pub">
+    Loading...
+</div>
+
+<script src="/script-google-doc.js"></script>
+```
+
+**Gotchas:** Make sure you use the published url and not the document's edit url with the `data-gdoc-url` attribute!
+
---
+++ script-google-doc.js
@@ -0,0 +1,55 @@
+/**
+ * Name: Google Doc Loading Script
+ * Author: Drew Hornbein
+ * 
+ * This script replaces a html element with the HTML content of a *published* Google Document
+ * Checkout the README to learn how to setup your Google Doc
+ * Add id="doc" to the element you wish to replace
+ * Add a data variable data-gdoc-url=""" to the element with the Google Doc PUBLISHED URL
+ * this script will extract the ID
+ * 
+ * Example:
+ * <div id="doc" data-gdoc-url="https://docs.google.com/document/d/e/2PACX-1vT5gCxJy7b1abHTQ0AKOFCYbssHDy1pVQyjJmBvsRrwA1T7GiULwaENsv2k_Mfwj8xYdBEiQzvJtD8N/pub">Loading...</div>
+ */
+
+// target element ID name
+// TODO: allow for overwrite
+let targetEl = 'doc';
+// target element
+var d = document.getElementById(targetEl);
+
+if (d && d.dataset.gdocUrl) {
+    request(d.dataset.gdocUrl);
+}
+
+// TODO: do something if no url is provided
+
+/**
+ * Request - loads google doc
+ * @param {string} url the publish URL of a Google Doc
+ */
+function request(url) {
+    // build iframe to load as a backup
+    const iframe = '<iframe class="doc" src="' + url + '?embedded=false"></iframe>';
+
+    // CORS request
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        // create html node
+        let html = document.createElement('html');
+        // add response to node
+        html.innerHTML = xhr.responseText;
+        // grab only the contents from the returned Google Doc html
+        d.innerHTML = html.getElementsByTagName('div')["contents"].innerHTML;
+        // add a class to loading div
+        d.classList.add("wrapper");
+    };
+    xhr.onerror = function () {
+        // add class to body 
+        document.body.classList.add("iframe");
+        // insert iframe into element
+        d.innerHTML = iframe;
+    }
+    xhr.send();
+}
